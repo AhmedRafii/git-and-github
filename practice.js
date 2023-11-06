490c6a8c94c2c6ffc06efdb1b10af8f6ea683af5
Desperate <capsicumlb@gmail.com>
2023-11-07 02:22:36 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,15 @@
+const accountId = 1444556
+let accountName = "Ahmed"
+var accountEmail = "rafi@rafi.com"
+accountCity = "Chittagong"
+
+// console.log(accountId);
+// console.log(accountName);
+// console.log(accountEmail);
+// console.log(accountCity);
+
+accountName = "Mohammad"
+accountEmail = "rafikhan78836@gmail.com"
+accountCity = "dhaka"
+
+console.table([accountId, accountName, accountEmail, accountCity ])
